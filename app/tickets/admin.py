0f51d9cb4869ab438489319742fe0269565ddd34
veni <viniciusstroher@gmail.com>
2021-10-25 23:02:55 -0300

    ajuste empty_label

--- app/tickets/admin.py
+++ app/tickets/admin.py
@@ -1,6 +1,36 @@
 from django.contrib import admin
 from .models import Quera, Ticket
 
+
+class QueraAdmin(admin.ModelAdmin):
+    fields = ['name']
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'get_author_name']
+    fields = ['title', 'author', 'content'] #author é referencia
+    readonly_fields = ['created_on']
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super(TicketAdmin, self).get_form(request, obj, **kwargs)
+        authorField = form.base_fields["author"]
+        authorField.widget.can_add_related = True
+        authorField.widget.can_change_related = False
+        authorField.widget.can_delete_related = False
+        authorField.empty_label = 'No value'
+
+        return form
+
+    # def get_queryset(self, request):
+    #     return super(TicketAdmin,self).get_queryset(request).select_related('book')
+
+    # sem performance
+    @admin.display(
+        ordering='author__name',
+        description='Nome do author',
+    )
+    def get_author_name(self, obj):
+        return obj.author.name or "Sem Author"
 # Register your models here.
-admin.site.register(Quera)
-admin.site.register(Ticket)
+admin.site.register(Quera, QueraAdmin)
+admin.site.register(Ticket, TicketAdmin)
+
+
--- app/tickets/models.py
+++ app/tickets/models.py
@@ -4,9 +4,13 @@ from django.db import models
 
 class Quera(models.Model):
     name = models.CharField(max_length=255)
-
+    def __str__(self):
+        return self.name
 class Ticket(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(Quera, on_delete=models.DO_NOTHING)
+    
+    def __str__(self):
+        return self.title
